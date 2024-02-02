40fce37ed7511cec58ca982ff4618ef44a1d2d3f
gilberthappi <gdushimimana6@gmail.com>
2024-02-02 16:30:16 +0200

    Update ReadMe and Fix linters

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+greeting1 = Greeting.create(greeting: "Hello")
+greeting2 = Greeting.create(greeting: "hola")
+greeting3 = Greeting.create(greeting: "Bonjour")
+greeting4 = Greeting.create(greeting: "Namaste")
+greeting5 = Greeting.create(greeting: "Habali")
